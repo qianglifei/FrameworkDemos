a1e9fe0f9af9f927adc1b8862d189006ef2c71ad
qianglifei <qianglifei99>
2020-08-26 17:53:14 +0800

    组件化架构 初次提交 Initial commit

---
+++ app/src/main/java/com/bksx/mobile/frameworkdemos/App.java
@@ -0,0 +1,12 @@
+package com.bksx.mobile.frameworkdemos;
+
+import android.app.Application;
+
+import com.bksx.mobile.common.BaseApplication;
+
+/**
+ * @author :qlf
+ */
+public class App extends BaseApplication {
+
+}
---
+++ common/src/main/java/com/bksx/mobile/common/BaseApplication.java
@@ -0,0 +1,57 @@
+package com.bksx.mobile.common;
+
+import android.app.Application;
+import android.content.res.Configuration;
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+
+import com.alibaba.android.arouter.launcher.ARouter;
+
+public class BaseApplication extends Application {
+
+    private  static  String TAG = "LifeCycleApplication";
+    
+    private  boolean isDebugARouter = true;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        // 程序创建的时候执行
+        Log.d(TAG, "onCreate: ");
+        if (isDebugARouter){
+            ARouter.openLog();
+            ARouter.openDebug();
+        }
+
+        ARouter.init(this);
+    }
+
+    @Override
+    public void onTerminate() {
+        //程序终止的时候执行
+        Log.d(TAG, "onTerminate: ");
+        super.onTerminate();
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        //低内存的时候执行
+        Log.d(TAG,"onLowMemory:");
+    }
+
+    @Override
+    public void onTrimMemory(int level) {
+        super.onTrimMemory(level);
+        //程序在内存清理的时候执行 （回收内存）
+        //HOME键退出应用程序，长按MENU键，打开Recent TASK 都会执行
+        Log.d(TAG,"onTrimMemory");
+    }
+
+    @Override
+    public void onConfigurationChanged(@NonNull Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        Log.d(TAG, "onConfigurationChanged: ");
+    }
+}
